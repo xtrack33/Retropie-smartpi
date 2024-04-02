3dda708ba7772d0c9d5694d95cfc9017530ecbec
Yumi-Lab <nicolas@yumi-lab.com>
2024-04-02 15:50:19 +0200

    update

--- install.sh
+++ install.sh
@@ -8,21 +8,8 @@ sudo apt-get update && sudo apt-get upgrade -y
 echo "Installing dependencies..."
 sudo apt-get install git dialog unzip xmlstarlet libsdl2-2.0-0 libsdl2-gfx-1.0-0 libsdl2-image-2.0-0 libsdl2-mixer-2.0-0 libsdl2-net-2.0-0 libsdl2-ttf-2.0-0 libsdl2-dev libusb-1.0-0-dev libavformat-dev libavdevice-dev pulseaudio samba xorg openbox alsa-utils menu libglib2.0-bin python3-xdg at-spi2-core dbus-x11 xmlstarlet joystick triggerhappy -y && echo "Dependencies installed successfully." || echo "Failed to install dependencies."
 
-# Cloning the Retropie-smartpi repository
-echo "Cloning the Retropie-smartpi repository..."
-if git clone https://github.com/Yumi-Lab/Retropie-smartpi.git; then
-    echo "Repository cloned successfully."
-else
-    echo "Failed to clone repository."
-    #exit 1
-fi
-
-# Changing to the cloned directory
-cd Retropie-smartpi || exit
-echo "Changed to the Retropie-smartpi directory."
-
 # Making the installation scripts executable
-echo "Making installation scripts executable..."
+#echo "Making installation scripts executable..."
 #sudo chmod +x retropie_setup.sh SambaRetropie.sh retropie_packages.sh autostartES.sh
 
 # Function to execute a script with arguments and check its success
